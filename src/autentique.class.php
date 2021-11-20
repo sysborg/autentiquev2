77ef0f4ad619dfc4ed5f133f880d7d9d52a9096b
Anderson Matheus Arruda <andmarruda@gmail.com>
2021-11-20 00:05:10 -0300

    Update autentique.class.php
    en-US: Removed autoloader from this file to autoloader.php
    pt-BR: Removido o autoloader desse arquivo para o autoloader.php

--- src/autentique.class.php
+++ src/autentique.class.php
@@ -2,6 +2,8 @@
     namespace sysborg\autentiquev2;
 
     class autentique{
+        use utils;
+
         /**
          * @description-en-US:       Stores API's informations
          * @description-pt-BR:       Armazena as informações da API
@@ -11,16 +13,6 @@
             'url'       => 'https://api.autentique.com.br/v2/graphql',
             'token'     => NULL,
             'devMode'   => false
-        ];
-
-        /**
-         * @description-en-US:       Store path for autoload
-         * @description-pt-BR:       Armazena caminhos para o autoload
-         * @var                      array
-         */
-        private static array $loadPath = [
-            'sysborg\autentiquev2\layouts' => '/layouts.interface.php',
-            'sysborg\autentiquev2\listDir' => '/layouts/dir/listDir.class.php'
         ];
 
         /**
@@ -35,20 +27,6 @@
         public function __construct(private \sysborg\autentiquev2\layouts $layout){}
 
         /**
-         * @description-en-US       Get class's paths
-         * @description-pt-BR       Pega os caminhos da classe
-         * @author                  Anderson Arruda < andmarruda@gmail.com >
-         * @version                 1.0.0
-         * @access                  public
-         * @param                   string $className
-         * @return                  string
-         */
-        public static function getClassPath(string $className) : string
-        {
-            return self::$loadPath[$className];
-        }
-
-        /**
          * @description-en-US       Set values to API's informations
          * @description-pt-BR       Seta valores para as informações da API
          * @author                  Anderson Arruda < andmarruda@gmail.com >
@@ -60,9 +38,7 @@
          */
         public function __set(string $apiInfoName, $val)
         {
-            $keys = array_keys($this->apiInfo);
-            if(array_search($apiInfoName, $keys)===-1)
-                throw new \Exception('en-US: No variable '. $apiInfoName. ' has been founded! Verify the name and try again! | pt-BR: Nenhuma variável com o nome '. $apiInfoName. ' foi encontrada! Verifique o nome e tente novamente!');
+            $this->verifyColumn($this->apiInfo, $apiInfoName);
 
             if($apiInfoName==='devMode' && !is_bool($val))
                 throw new \Exception('en-US: devMode expects boolean and '. gettype($val). ' are given! | pt-BR: devMode espera boolean e '. gettype($val). ' foi passado!');
@@ -81,10 +57,7 @@
          */
         public function __get(string $apiInfoName)
         {
-            $keys = array_keys($this->apiInfo);
-            if(array_search($apiInfoName, $keys)===-1)
-                throw new \Exception('en-US: No variable '. $apiInfoName. ' has been founded! Verify the name and try again! | pt-BR: Nenhuma variável com o nome '. $apiInfoName. ' foi encontrada! Verifique o nome e tente novamente!');
-
+            $this->verifyColumn($this->apiInfo, $apiInfoName);
             return $this->apiInfo[$apiInfoName];
         }
 
@@ -115,8 +88,4 @@
             return $r;
         }
     }
-
-    spl_autoload_register(function($cls){
-        require_once __DIR__. \sysborg\autentiquev2\autentique::getClassPath($cls);
-    });
 ?>
