fbc3f8a6c5af4d51ce0e7a5f3300d193a6a1c4dc
Anderson Matheus Arruda <andmarruda@gmail.com>
2021-11-20 17:40:11 -0300

    Create common.class.php

---
+++ src/layouts/common.class.php
@@ -0,0 +1,38 @@
+<?php
+    namespace sysborg\autentiquev2;
+
+    abstract class common{
+        use utils;
+
+        /**
+         * @description-en-US       Set expected and possible values to the layout
+         * @description-pt-BR       Seta valores esperados e possíveis ao layout
+         * @author                  Anderson Arruda < andmarruda@gmail.com >
+         * @version                 1.0.0
+         * @access                  public
+         * @param                   string $apiInfoName
+         * @param                   mixed  $val
+         * @return                  void
+         */
+        public function __set(string $layoutInfoName, $val)
+        {
+            $this->verifyColumn($this->layoutInfo, $layoutInfoName);
+            $this->layoutInfo[$layoutInfoName] = $val;
+        }
+
+        /**
+         * @description-en-US       Get setted values of the layout
+         * @description-pt-BR       Pega valores setados ao layout
+         * @author                  Anderson Arruda < andmarruda@gmail.com >
+         * @version                 1.0.0
+         * @access                  public
+         * @param                   string $apiInfoName
+         * @return                  mixed
+         */
+        public function __get(string $layoutInfoName)
+        {
+            $this->verifyColumn($this->layoutInfo, $layoutInfoName);
+            return $this->layoutInfo[$layoutInfoName];
+        }
+    }
+?>
