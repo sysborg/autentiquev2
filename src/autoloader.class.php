f1167c07dd61a9796b07bd8c3864f415efeab24b
Anderson Matheus Arruda <andmarruda@gmail.com>
2021-11-20 00:03:24 -0300

    Add files via upload
    en-US: Creating an autoloader for the project.
    pt-BR: Criando um autoloader para o projeto.

---
+++ src/autoloader.class.php
@@ -0,0 +1,35 @@
+<?php
+    namespace sysborg\autentiquev2;
+
+    final class autoloader{
+        /**
+         * @description-en-US:       Store path for autoload
+         * @description-pt-BR:       Armazena caminhos para o autoload
+         * @var                      array
+         */
+        private static array $loadPath = [
+            'sysborg\autentiquev2\layouts'      => '/layouts.interface.php',
+            'sysborg\autentiquev2\listDir'      => '/layouts/dir/listDir.class.php',
+            'sysborg\autentiquev2\utils'        => '/utils.traits.php',
+            'sysborg\autentiquev2\autentique'   => '/autentique.class.php'
+        ];
+
+        /**
+         * @description-en-US       Get class's paths
+         * @description-pt-BR       Pega os caminhos da classe
+         * @author                  Anderson Arruda < andmarruda@gmail.com >
+         * @version                 1.0.0
+         * @access                  public
+         * @param                   string $className
+         * @return                  string
+         */
+        public static function getClassPath(string $className) : string
+        {
+            return self::$loadPath[$className];
+        }
+    }
+
+    spl_autoload_register(function($cls){
+        require_once __DIR__. \sysborg\autentiquev2\autoloader::getClassPath($cls);
+    });
+?>
