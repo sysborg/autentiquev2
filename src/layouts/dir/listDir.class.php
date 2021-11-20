cf824e8a76f1d9557bbbf061d416f9e623241873
Anderson Matheus Arruda <andmarruda@gmail.com>
2021-11-20 17:41:17 -0300

    Update listDir.class.php

--- src/layouts/dir/listDir.class.php
+++ src/layouts/dir/listDir.class.php
@@ -1,9 +1,7 @@
 <?php
     namespace sysborg\autentiquev2;
 
-    class listDir implements \sysborg\autentiquev2\layouts{
-        use utils;
-
+    class listDir extends common implements \sysborg\autentiquev2\layouts{
         /**
          * @description-en-US:       Stores informations and variables for this layout
          * @description-pt-BR:       Armazena informações e variáveis para esse layout
@@ -23,37 +21,6 @@
             "query": "query { folders(limit: %d, page: %d) { total data { id name type created_at } } }",
             "variables": {}
         }';
-
-        /**
-         * @description-en-US       Set expected and possible values to the layout
-         * @description-pt-BR       Seta valores esperados e possíveis ao layout
-         * @author                  Anderson Arruda < andmarruda@gmail.com >
-         * @version                 1.0.0
-         * @access                  public
-         * @param                   string $apiInfoName
-         * @param                   mixed  $val
-         * @return                  void
-         */
-        public function __set(string $layoutInfoName, $val)
-        {
-            $this->verifyColumn($this->layoutInfo, $layoutInfoName);
-            $this->layoutInfo[$layoutInfoName] = $val;
-        }
-
-        /**
-         * @description-en-US       Get setted values of the layout
-         * @description-pt-BR       Pega valores setados ao layout
-         * @author                  Anderson Arruda < andmarruda@gmail.com >
-         * @version                 1.0.0
-         * @access                  public
-         * @param                   string $apiInfoName
-         * @return                  mixed
-         */
-        public function __get(string $layoutInfoName)
-        {
-            $this->verifyColumn($this->layoutInfo, $layoutInfoName);
-            return $this->layoutInfo[$layoutInfoName];
-        }
 
         /**
          * @description-en-US       Parse the values on the graphql schema and returns as string
