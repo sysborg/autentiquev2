3e7711595f531590f3aa15d8ca0b5aed1eaa2327
Anderson Matheus Arruda <andmarruda@gmail.com>
2021-11-20 17:42:53 -0300

    Create deleteDir.class.php

---
+++ src/layouts/dir/deleteDir.class.php
@@ -0,0 +1,39 @@
+<?php
+    namespace sysborg\autentiquev2;
+
+    class deleteDir extends common implements \sysborg\autentiquev2\layouts{
+        /**
+         * @description-en-US:       Stores informations and variables for this layout
+         * @description-pt-BR:       Armazena informações e variáveis para esse layout
+         * @var                      array
+         */
+        private array $layoutInfo = [
+            'id' => NULL
+        ];
+
+        /**
+         * @description-en-US:       Stores the query of graphql
+         * @description-pt-BR:       Armazena a query do graphql
+         * @var                      string
+         */
+        private string $query = '{
+            "query": "mutation { deleteFolder(id: "%s") }",
+            "variables": {}
+        }';
+
+        /**
+         * @description-en-US       Parse the values on the graphql schema and returns as string
+         * @description-pt-BR       Parse os valores no schema do graphql e retorna como string
+         * @author                  Anderson Arruda < andmarruda@gmail.com >
+         * @version                 1.0.0
+         * @access                  public
+         * @param                   
+         * @return                  string
+         */
+        public function parse() : string
+        {
+            $this->verifyFill('id', $this->id);
+            return sprintf($this->query, $this->id);
+        }
+    }
+?>
