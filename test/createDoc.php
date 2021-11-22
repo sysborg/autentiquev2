14264f717012e531d0f2daf552b1d4b3d6f892de
Anderson Matheus Arruda <andmarruda@gmail.com>
2021-11-22 19:26:16 -0300

    Create createDoc.php

---
+++ test/createDoc.php
@@ -0,0 +1,33 @@
+<?php
+    require_once __DIR__. '/../src/autoloader.php';
+    use sysborg\autentiquev2\createDoc;
+    use sysborg\autentiquev2\autentique;
+    
+    $token = ''; //en-US: put your autentique's token here https://www.autentique.com.br/ | pt-BR: coloque seu token da autentique aqui https://www.autentique.com.br/
+
+    /**
+     * en-US: Calling the desired layout and passing the variables expecteds and disred. At this case the directory's list
+     * pt-BR: Invoca o layout desejado e passa as variáveis esperadas e desejadas. Nesse caso a listagem de diretórios
+     */
+    $l = new createDoc();
+    $l->name = 'Test with one signer';
+    $l->file = __DIR__. '/examples/doc1.pdf';
+    $signer = $l->addSigners('andmarruda@gmail.com', 80, 70, 1);
+
+    /**
+     * en-US: Invokes the autentique api to transmit data by curl and recive the response
+     * pt-BR: Invoca a api do autentique e transmite os dados por curl e recebe a resposta
+     */
+
+    $t = new autentique($l);
+    $t->debug=true;
+    $t->token=$token;
+    $r = $t->transmit();
+    echo '//en-US: Clean response | pt-BR: Resposta limpa<br><pre>';
+    var_dump($r); //en-US: Clean response | pt-BR: Resposta limpa
+    echo '</pre>';
+
+    echo '<br><br>//en-US: Response decoded | pt-BR: Resposta decodificada<br><pre>';
+    var_dump(json_decode($r, true)); //en-US: Response decoded | pt-BR: Resposta decodificada
+    echo '</pre>';
+?>
