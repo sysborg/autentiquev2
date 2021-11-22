ef27a37c8405b43ed6b14a68933d7558e2f0795e
Anderson Matheus Arruda <andmarruda@gmail.com>
2021-11-22 18:51:08 -0300

    Update utils.traits.php

--- src/utils.traits.php
+++ src/utils.traits.php
@@ -47,7 +47,7 @@
          */
         public function verifyEmail(string $variableName, string $email) : void
         {
-            $exp = '/(?:[a-z0-9!#$%&\'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&\'*+\/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/g';
+            $exp = '/^([a-zA-Z0-9]{1,}[a-zA-Z0-9\\!\\#\\$\\%\\&\\*\\+\\-\\/\\=\\?\\^\\_\\`\\{\\|\\(\\)\\,\\:\\;\\<\\>\\@\\[\\\\\]]{1,}){1,65}\@[A-Za-z0-9\.\-]{1,}\.[a-z]{3}([a-z]{2,3})?$/';
             if(!preg_match($exp, $email))
                 throw new \Exception('en-US: Variable '. $variableName. ' must be a valid email, please verify the entry and try again! | pt-BR: A variável '. $variableName. ' deve ser um email válido, por favor verifique a entrada e tente novamente!');
         }
