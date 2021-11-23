6d86f5f3b41bd6cdc3d0b2ae413f1e2074d44248
Anderson Matheus Arruda <andmarruda@gmail.com>
2021-11-23 09:15:52 -0300

    Create signDoc.class.php

---
+++ src/layouts/doc/signDoc.class.php
@@ -0,0 +1,38 @@
+<?php
+    namespace sysborg\autentiquev2;
+
+    class signDoc extends common implements \sysborg\autentiquev2\layouts{
+        /**
+         * @description-en-US:       Stores informations and variables for this layout
+         * @description-pt-BR:       Armazena informações e variáveis para esse layout
+         * @var                      array
+         */
+        protected array $layoutInfo = [
+            'document_id' => ''
+        ];
+
+        /**
+         * @description-en-US:       Stores the query of graphql
+         * @description-pt-BR:       Armazena a query do graphql
+         * @var                      string
+         */
+        private string $query = '{
+            "query": "mutation { signDocument(id: \"%s\") }",
+            "variables": {}
+        }';
+
+        /**
+         * @description-en-US       Parse the values on the graphql schema and returns as string
+         * @description-pt-BR       Parse os valores no schema do graphql e retorna como string
+         * @author                  Anderson Arruda < andmarruda@gmail.com >
+         * @version                 1.0.0
+         * @access                  public
+         * @param                   
+         * @return                  string
+         */
+        public function parse() : string
+        {
+            return sprintf($this->query, $this->document_id, $this->folder_id);
+        }
+    }
+?>
