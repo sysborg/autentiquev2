d1d8b95dd03cc86156b47747004d69eb0bf50607
Anderson Matheus Arruda <andmarruda@gmail.com>
2021-11-27 15:56:23 -0300

    Update rescueDoc.class.php

--- src/layouts/doc/rescueDoc.class.php
+++ src/layouts/doc/rescueDoc.class.php
@@ -1,7 +1,7 @@
 <?php
     namespace sysborg\autentiquev2;
 
-    class rescueDoc extends common implements \sysborg\autentiquev2\layouts{
+    class signDoc extends common implements \sysborg\autentiquev2\layouts{
         /**
          * @description-en-US:       Stores informations and variables for this layout
          * @description-pt-BR:       Armazena informações e variáveis para esse layout
@@ -17,7 +17,7 @@
          * @var                      string
          */
         protected string $query = '{
-            "query": "query { document(id: \"%s\") { id name refusable sortable created_at files { original signed } signatures { public_id name email created_at action { name } link { short_link } user { id name email } email_events { sent opened delivered refused reason } viewed { ...event } signed { ...event } rejected { ...event } } } } fragment event on Event { ip port reason created_at geolocation { country countryISO state stateISO city zipcode latitude longitude } }",
+            "query": "mutation { signDocument(id: \"%s\") }",
             "variables": {}
         }';
 
