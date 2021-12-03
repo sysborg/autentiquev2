99eab1d36d47f6988e615705500840bfac922f05
Anderson Matheus Arruda <andmarruda@gmail.com>
2021-12-02 22:43:24 -0300

    Update autoloader.php

--- src/autoloader.php
+++ src/autoloader.php
@@ -8,21 +8,22 @@
          * @var                      array
          */
         private static array $loadPath = [
-            'sysborg\autentiquev2\layouts'      => '/layouts.interface.php',
-            'sysborg\autentiquev2\common'       => '/layouts/common.class.php',
-            'sysborg\autentiquev2\listDir'      => '/layouts/dir/listDir.class.php',
-            'sysborg\autentiquev2\createDir'    => '/layouts/dir/createDir.class.php',
-            'sysborg\autentiquev2\deleteDir'    => '/layouts/dir/deleteDir.class.php',
-            'sysborg\autentiquev2\rescueDir'    => '/layouts/dir/rescueDir.class.php',
-            'sysborg\autentiquev2\createDoc'    => '/layouts/doc/createDoc.class.php',
-            'sysborg\autentiquev2\deleteDoc'    => '/layouts/doc/deleteDoc.class.php',
-            'sysborg\autentiquev2\listDoc'      => '/layouts/doc/listDoc.class.php',
-            'sysborg\autentiquev2\listDocDir'   => '/layouts/doc/listDocDir.class.php',
-            'sysborg\autentiquev2\moveDoc'      => '/layouts/doc/moveDoc.class.php',
-            'sysborg\autentiquev2\signDoc'      => '/layouts/doc/signDoc.class.php',
-            'sysborg\autentiquev2\rescueDoc'    => '/layouts/doc/rescueDoc.class.php',
-            'sysborg\autentiquev2\utils'        => '/utils.traits.php',
-            'sysborg\autentiquev2\autentique'   => '/autentique.class.php'
+            'sysborg\autentiquev2\layouts'                  => '/layouts.interface.php',
+            'sysborg\autentiquev2\common'                   => '/layouts/common.class.php',
+            'sysborg\autentiquev2\listDir'                  => '/layouts/dir/listDir.class.php',
+            'sysborg\autentiquev2\createDir'                => '/layouts/dir/createDir.class.php',
+            'sysborg\autentiquev2\deleteDir'                => '/layouts/dir/deleteDir.class.php',
+            'sysborg\autentiquev2\rescueDir'                => '/layouts/dir/rescueDir.class.php',
+            'sysborg\autentiquev2\createDoc'                => '/layouts/doc/createDoc.class.php',
+            'sysborg\autentiquev2\deleteDoc'                => '/layouts/doc/deleteDoc.class.php',
+            'sysborg\autentiquev2\listDoc'                  => '/layouts/doc/listDoc.class.php',
+            'sysborg\autentiquev2\listDocDir'               => '/layouts/doc/listDocDir.class.php',
+            'sysborg\autentiquev2\moveDoc'                  => '/layouts/doc/moveDoc.class.php',
+            'sysborg\autentiquev2\signDoc'                  => '/layouts/doc/signDoc.class.php',
+            'sysborg\autentiquev2\rescueDoc'                => '/layouts/doc/rescueDoc.class.php',
+            'sysborg\autentiquev2\docSignaturePosition'     => '/layouts/doc/docSignaturePosition',
+            'sysborg\autentiquev2\utils'                    => '/utils.traits.php',
+            'sysborg\autentiquev2\autentique'               => '/autentique.class.php'
         ];
 
         /**
